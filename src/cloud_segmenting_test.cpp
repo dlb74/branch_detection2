5d0dbb748a83c32677d745513cd7df1b6d45e04b
Dale Baker <dlb74@uclive.ac.nz>
2017-09-09 23:31:51 +1200

    added method to find if points are in a cylinder or not, also cleaned up code a little bit - removed comments, removed hard coded resizing of trunk normals

--- src/cloud_segmenting_test.cpp
+++ src/cloud_segmenting_test.cpp
@@ -66,6 +66,68 @@ typedef pcl::Normal PointNT;
 #define TRUNKSEG_CYLRAD_MAX 1
 
 
+struct Vec3
+{
+    float x;
+    float y;
+    float z;
+};
+
+float CylTest_CapsFirst( const Vec3 & pt1, const Vec3 & pt2, float lengthsq, float radius_sq, const Vec3 & testpt )
+{
+    float dx, dy, dz;	// vector d  from line segment point 1 to point 2
+    float pdx, pdy, pdz;	// vector pd from point 1 to test point
+    float dot, dsq;
+
+    dx = pt2.x - pt1.x;	// translate so pt1 is origin.  Make vector from
+    dy = pt2.y - pt1.y;     // pt1 to pt2.  Need for this is easily eliminated
+    dz = pt2.z - pt1.z;
+
+    pdx = testpt.x - pt1.x;		// vector from pt1 to test point.
+    pdy = testpt.y - pt1.y;
+    pdz = testpt.z - pt1.z;
+
+    // Dot the d and pd vectors to see if point lies behind the
+    // cylinder cap at pt1.x, pt1.y, pt1.z
+
+    dot = pdx * dx + pdy * dy + pdz * dz;
+
+    // If dot is less than zero the point is behind the pt1 cap.
+    // If greater than the cylinder axis line segment length squared
+    // then the point is outside the other end cap at pt2.
+
+    if( dot < 0.0f || dot > lengthsq )
+    {
+        return( -1.0f );
+    }
+    else
+    {
+        // Point lies within the parallel caps, so find
+        // distance squared from point to line, using the fact that sin^2 + cos^2 = 1
+        // the dot = cos() * |d||pd|, and cross*cross = sin^2 * |d|^2 * |pd|^2
+        // Carefull: '*' means mult for scalars and dotproduct for vectors
+        // In short, where dist is pt distance to cyl axis:
+        // dist = sin( pd to d ) * |pd|
+        // distsq = dsq = (1 - cos^2( pd to d)) * |pd|^2
+        // dsq = ( 1 - (pd * d)^2 / (|pd|^2 * |d|^2) ) * |pd|^2
+        // dsq = pd * pd - dot * dot / lengthsq
+        //  where lengthsq is d*d or |d|^2 that is passed into this function
+
+        // distance squared to the cylinder axis:
+
+        dsq = (pdx*pdx + pdy*pdy + pdz*pdz) - dot*dot/lengthsq;
+
+        if( dsq > radius_sq )
+        {
+            return( -1.0f );
+        }
+        else
+        {
+            return( dsq );		// return distance squared to axis
+        }
+    }
+}
+
 /** PCL Frame Normal Estimation */
 void Norm_Est( pcl::PointCloud<PointT>::Ptr cloud, pcl::PointCloud<PointNT>::Ptr cloud_normals, float normKSearchRadius )
 {
@@ -146,40 +208,13 @@ void Cylinder_Seg( pcl::PointCloud<PointT>::Ptr cloud,
     extract.filter (*cloud_remainder);
 }
 
-void Generate_Pointcloud(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud) {
-    // Fill in the cloud data
-    cloud->width  = 150;
-    cloud->height = 1;
-    cloud->points.resize (cloud->width * cloud->height);
-
-    // Generate the data
-    for (size_t i = 0; i < cloud->points.size (); ++i)
-    {
-        cloud->points[i].x = -100 + (rand () % 200);
-        cloud->points[i].y = -100 + (rand () % 200);
-        cloud->points[i].z = rand () % 50;
-    }
-
-    // Set a few outliers
-    cloud->points[0].x = -120.0;
-    cloud->points[3].x = -120.0;
-    cloud->points[6].x = -120.0;
-    cloud->points[0].z = -115.0;
-    cloud->points[3].z = -125.0;
-    cloud->points[6].z = -135.0;
-}
-
-
 
 int main(int argc, char **argv)
 {
-
     pcl::PointCloud<PointT>::Ptr cloud_blob (new pcl::PointCloud<PointT>);
-    //pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
 
     // Fill in the cloud data
     pcl::PCDReader reader;
-    //reader.read ("cloud_filtered.pcd", *cloud_blob);
     reader.read ("/home/cosc/student/dlb74/catkin_ws/src/branch_detection2/src/cloud_filtered.pcd", *cloud_blob);
 
     int num_points = cloud_blob->size();
@@ -190,7 +225,7 @@ int main(int argc, char **argv)
     pcl::ModelCoefficients::Ptr coefficients_cylinder_trunk (new pcl::ModelCoefficients);
     coefficients_cylinder_trunk->values.resize (7);
     pcl::PointCloud<PointNT>::Ptr trunk_normals (new pcl::PointCloud<PointNT>);
-    trunk_normals->points.resize(1496064);
+    trunk_normals->points.resize(cloud_blob->size());
     pcl::PointCloud<PointT>::Ptr cloud_after_trunk_seg (new pcl::PointCloud<PointT> ());
 
     string str = "";
@@ -220,16 +255,6 @@ int main(int argc, char **argv)
     viewer2->setBackgroundColor (0, 0, 0);
     viewer2->addPointCloud<PointT> (cloud_after_trunk_seg, "Filtered Cloud");
 
-    //cout << coefficients_cylinder_trunk->values[1] << "t" << coefficients_cylinder_trunk->values[4] << "s";
-    //0.195297t0.0747359s
-
-//1,0,3
-    //coefficients_cylinder_trunk->values[3] = -1.503712;
-    //coefficients_cylinder_trunk->values[4] = -0.8505282;
-    //coefficients_cylinder_trunk->values[5] = 2.593391;
-    //coefficients_cylinder_trunk->values[6] = 0.4;
-
-
     while (!viewer->wasStopped ())
     {
 
@@ -238,14 +263,6 @@ int main(int argc, char **argv)
             // << coefficients_cylinder_trunk->values[4] << "e" << coefficients_cylinder_trunk->values[5] << "f"
              //<< coefficients_cylinder_trunk->values[6] << "g";
         // 2.67866a1b-0.981821c0.587474d0.0747359e0.805785f0.275014g
-
-        //2.67866a1b-0.981821c0.587474d0.0747359e0.805785f0.275014g
-        //A = (2.67866, 1, -0.981821)   B = (0.587474, 0.0747359, 0.805785)
-
-        //AB = (-2.091186, -0.9252641, 1.787606)
-
-        //A - 2AB = (-1.503712, -0.8505282, 2.593391)
-
 
         viewer->removeAllShapes();
         viewer->updatePointCloud(cloud_filtered, "Filtered Cloud");
@@ -258,21 +275,6 @@ int main(int argc, char **argv)
         viewer2->spinOnce(100);
 
     }
-    //pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    //Generate_Pointcloud(cloud);
-
-    /*
-    Cylinder_Seg( pcl::PointCloud<PointT>::Ptr cloud,
-            pcl::PointCloud<PointNT>::Ptr cloud_normals,
-            pcl::ModelCoefficients::Ptr coefficients_cylinder,
-            pcl::PointCloud<PointT>::Ptr cloud_remainder,
-            double normalWeight, double distanceThreshold, double radiusMinimum, double radiusMaximum,
-            int modelType)
-*/
-
-
-
-
 
     return (0);
 
